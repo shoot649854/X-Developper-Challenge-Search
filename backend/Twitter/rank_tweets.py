0107f3ff497b822e203f58b0e94507d1d492df28
Saarth Shah <69341449+SaarthShah@users.noreply.github.com>
2024-04-21 09:26:43 -0700

    changed to open ai embeddings

--- backend/Twitter/rank_tweets.py
+++ backend/Twitter/rank_tweets.py
@@ -1,12 +1,12 @@
 from gensim.models import Word2Vec
 from sklearn.metrics.pairwise import cosine_similarity
 import json
+from backend.Twitter.process_queries import generate_embeddings
 
 def similarity_scorer(tokenized_text1, tokenized_text2):
-    model = Word2Vec([tokenized_text1, tokenized_text2], min_count=1, vector_size=100)
-    tweet_embeddings = [model.wv[word] for word in tokenized_text1]
-    context_string_embeddings = [model.wv[word] for word in tokenized_text2]
-    similarity_score = cosine_similarity([sum(tweet_embeddings)], [sum(context_string_embeddings)])
+    embeddings1 = generate_embeddings(' '.join(tokenized_text1))
+    embeddings2 = generate_embeddings(' '.join(tokenized_text2))
+    similarity_score = cosine_similarity([embeddings1], [embeddings2])
     return similarity_score[0][0]
 
 def rank_tweets(queries: list, context_string: str):
@@ -18,7 +18,11 @@ def rank_tweets(queries: list, context_string: str):
             
             for tweet in tweets:
                 tokenized_tweet = tweet["text"].split()
-                ranked_tweets.append((similarity_scorer(tokenized_tweet, tokenized_context_string), tweet))
+                score = similarity_scorer(tokenized_tweet, tokenized_context_string)
+                if score < 0.35:
+                    continue
+                else:
+                    ranked_tweets.append((score, tweet))
         except:
             continue
     
