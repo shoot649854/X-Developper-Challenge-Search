f972a95bb33772c4ceac9b00a5827cf6f46e2d47
Saarth Shah <69341449+SaarthShah@users.noreply.github.com>
2024-04-21 00:03:10 -0700

    cleaned code

--- backend/main.py
+++ backend/main.py
@@ -9,22 +9,6 @@ from Twitter.process_queries import analyze
 from Twitter.rank_tweets import rank_tweets
 
 DATA_DIR = os.path.join(os.getcwd(), "data")
-# subqueries = [
-#     "NBA Playoffs 2024",
-#     "NBA Playoffs Bracket 2024"
-# ]
-
-# data = {"queries": []}
-
-# for item in subqueries:
-#     query = "#{0} -is:retweet".format(item)
-#     search_result = recent_search(query, 10)
-#     formatted_result = json.dumps(json.loads(search_result), indent=4)
-#     data["queries"].append({"query": query, "results": formatted_result})
-
-# directory = os.path.join(DATA_DIR, "recent_search_queries.json")
-# with open(directory, "w") as json_file:
-#     json.dump(data, json_file, indent=4)
 
 
 def get_results(query):
@@ -39,6 +23,3 @@ def get_results(query):
         data["queries"].append({"query": query, "results": formatted_result})
     sorted_tweets = rank_tweets(data["queries"], description)
     return (res, data)
-
-if __name__ == "__main__":
-    print(get_results("NBA Playoffs 2024"))
